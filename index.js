a8db33ebd6683d6f717972413cef54543bdd7c85
Dileepa Sandaruwan Mabulage <99941065+dsmabulage@users.noreply.github.com>
2025-04-08 15:45:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Sample Nodejs App');
+  const envName = process.env.ENV_NAME || 'DefaultApp';
+  res.send(`Welcome to ${envName}`);
 });
 
 app.get('/users', (req, res) => {
